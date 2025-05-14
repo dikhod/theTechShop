c4b333bbec824231f6e964637fe4abb2e9f114c9
dikhod <dilshadkhan.7ind@gmail.com>
2025-05-14 14:04:06 +0530

    first commit

---
+++ src/products.js
@@ -0,0 +1,66 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.webp";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.png";
+import product7 from "./assets/products/7.webp";
+import product8 from "./assets/products/8.webp";
+
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Apple IPhone 14",
+    price: 999.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Macbook Pro 2022 (M1)",
+    price: 1999.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Cannon M50 Camera",
+    price: 699.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Apple IPad",
+    price: 499.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "LED Light Strips",
+    price: 19.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Beats by Dre Headphones",
+    price: 199.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Earbuds",
+    price: 99.0,
+    productImage: product8,
+  },
+  {
+    id: 8,
+    productName: "Mackbook Pro",
+    price: 599.0,
+    productImage: product2,
+  },
+  {
+    id: 9,
+    productName: "Alexa",
+    price: 399.0,
+    productImage: product7,
+  },
+];
